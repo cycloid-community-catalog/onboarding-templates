e02433c86f6cc28aff68a13e11bd267d728fad71
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-05-26 18:09:02 +0200

    fix gcp provider

--- template-nexus-repository/terraform/gcp/variables.tf
+++ template-nexus-repository/terraform/gcp/variables.tf
@@ -5,6 +5,10 @@ variable "customer" {}
 
 # GCP variables
 variable "gcp_project" {}
+variable "gcp_region" {
+  description = "GCP region where to create the infrastructure."
+  default     = "europe-west1"
+}
 variable "gcp_zone" {
   description = "GCP zone where to create the infrastructure."
   default     = "europe-west1-b"
