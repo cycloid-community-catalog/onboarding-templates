cbdcdb0db7521a3bec51c5e25161eae4ee995ec6
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-05-26 17:42:05 +0200

    fix gcp provider

--- template-nexus-repository/terraform/gcp/provider.tf
+++ template-nexus-repository/terraform/gcp/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project = var.gcp_project
-  region  = substr(var.gcp_zone, 0, length("${var.gcp_zone}")-3)
-  zone  = var.gcp_zone
+  region  = substr(var.gcp_zone, 0, length(var.gcp_zone)-3)
+  zone    = var.gcp_zone
 }
--- template-nodejs/terraform/gcp/variables.tf
+++ template-nodejs/terraform/gcp/variables.tf
@@ -6,10 +6,6 @@ variable "customer" {}
 # GCP variables
 variable "gcp_project" {}
 variable "gcp_zone" {
-  description = "GCP zone where to create servers."
-  default     = "b"
-}
-variable "gcp_region" {
-  description = "GCP region where to create servers."
-  default     = substr(var.gcp_zone, 0, length(var.gcp_zone)-3)
+  description = "GCP zone where to create the infrastructure."
+  default     = "europe-west1-b"
 }
