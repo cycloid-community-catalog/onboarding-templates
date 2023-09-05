08beebc47a1dd50505129d284ad08bd2d7b67e4e
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-09-05 17:24:20 +0200

    fix ansible stack aws use case

--- template-terraform-ansible-nexus/terraform/aws/variables.tf
+++ template-terraform-ansible-nexus/terraform/aws/variables.tf
@@ -4,9 +4,8 @@ variable "project" {}
 variable "customer" {}
 
 # AWS variables
-variable "aws_cred" {
-  description = "Contains AWS access_key and secret_key"
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "aws_region" {
   description = "AWS region where to create servers."
   default     = "eu-west-1"
