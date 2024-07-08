b37fe5267857b1b417c91991bbbac7194fd80782
olivier2t <80399237+olivier2t@users.noreply.github.com>
2024-07-08 18:25:49 +0200

    Update variables.tf

--- template-terraform/terraform/aws/variables.tf
+++ template-terraform/terraform/aws/variables.tf
@@ -11,17 +11,3 @@ variable "aws_region" {
   description = "AWS region where to create servers."
   default     = "eu-west-1"
 }
-
-# Networking
-variable "edit_subnet" {
-  description = "Whether to use the Cycloid Asset Inventory to select a Subnet ID, or to enter it manually. To populate your subnet in Cycloid Inventory, you may want to use the Networking stack template."
-  default     = false
-}
-variable "inventory_subnet_id" {
-  description = "Subnet ID where to deploy the instance. Must belong to the region stated in this form."
-  default     = ""
-}
-variable "edit_subnet_id" {
-  description = "Subnet ID where to deploy the instance. Must belong to the region stated in this form."
-  default     = ""
-}
